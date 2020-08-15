31e3f3ce20ef0879e655fa14f0d86ed0d6ba1f12
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-08-14 23:44:19 -0300

    mejoras aleatorias

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
     var instances = M.Sidenav.init(elems, options);
-  });
+});
+
+$(document).ready(function () {
+    
+    $(".slicking").slick({
+        centerMode: true,
+        centerPadding: "60px",
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: "40px",
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+});
