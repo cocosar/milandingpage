baa2f21df0f2341d99de51d9c1a1b3eb28407fa6
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-08-14 23:59:27 -0300

    agregada mq

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".sidenav");
-    var instances = M.Sidenav.init(elems, options);
-});
+
 
 $(document).ready(function () {
     $(function () {
